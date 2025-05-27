2ec87dc0aeac06f1d11e81861e845fd9816d51ef
snow011307 <egawayuuki0113@gmail.com>
2025-05-27 21:28:34 +0900

    【追加】プラグインのMainを追加

---
+++ src/main/java/snowsan0113/mcgame/Main.java
@@ -0,0 +1,17 @@
+package snowsan0113.mcgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
